04feef67bbb0b2ddf0a63ccb8f5c48bab7de918f
System Administrator <root@NTB-158.local>
2020-06-08 22:05:37 -0400

    Terminando de animar todos los elementos de la interfaz y tambien todos los estados

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.scss';
 import { Route } from 'react-router-dom';
+import { CSSTransition} from 'react-transition-group';
+import { gsap } from 'gsap';
 
 //Pages
 import About from './pages/about';
@@ -15,15 +17,56 @@ const routes = [
 ];
 
 function App() {
+
+  const onEnter = node => {
+    gsap.from(
+      [node.children[0].firstElementChild, node.children[0].lastElementChild],
+      0.6,
+      {
+        y: 30,
+        delay: 0.6,
+        ease: "power3.InOut",
+        opacity: 0,
+        stagger: {
+          amount: 0.6
+        }
+      }
+    );
+  };
+
+  const onExit = node => {
+    gsap.to(
+      [node.children[0].firstElementChild, node.children[0].lastElementChild],
+      0.6,
+      {
+        y: -30,
+        ease: "power3.InOut",
+        stagger: {
+          amount: 0.2
+        }
+      }
+    );
+  };
+  
   return (
     <>
     <Header />
       <div className="container">
         { routes.map (({ path, Component }) => (
           <Route key='name' path={path} exact>
-            <div className="page">
-              <Component />
-            </div>
+            {({ match }) => (
+              <CSSTransition
+                in={match != null}
+                timeout={1200}
+                classNames='page'
+                onExit={onExit}
+                onEntering={onEnter}
+                unmountOnExit>
+                <div className='page'>
+                  <Component />
+                </div>
+              </CSSTransition>
+            )}
           </Route>
         ))}
       </div>
